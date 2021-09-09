8a0badfd6ef3cfd35088149fa7d074c6090ee99e
mai0809 <Maitrayee.Kar@fnf.com>
2021-09-09 14:40:39 +0530

    Add project files.

---
+++ SamplePractice/Demo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SamplePractice
+{
+    class Demo
+    {
+
+        static void Main(string[] args)
+        {
+
+            double num1;
+            double num2;
+            string operand;
+            double answer;
+
+            Console.Write("Please enter the first integer: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter an operand (+, -, /, *): ");
+            operand = Console.ReadLine();
+
+            Console.Write("Please enter the second integer: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (operand)
+            {
+
+                case "-":
+                    answer = num1 - num2;
+                    break;
+
+                case "+":
+                    answer = num1 + num2;
+                    break;
+
+                case "/":
+                    answer = num1 / num2;
+                    break;
+
+                case "*":
+                    answer = num1 * num2;
+                    break;
+
+                default:
+                    answer = 0;
+                    break;
+            }
+            Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ SamplePractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplePractice
+{
+    
+
+namespace SampleTest
+    {
+        class NewClass
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the Name please");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the Age please");
+                string age = Console.ReadLine();
+
+                Console.WriteLine("Enter the salary");
+                double salary = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("the name is {0} from age {1} whose qualification {2}", name, age, salary);
+            }
+        }
+    }
+}
+
